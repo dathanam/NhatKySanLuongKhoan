811cd708a72f4075199b86375485ba143b969490
thuccd <62067426+thuccd@users.noreply.github.com>
2021-10-31 06:50:06 +0700

    Tạo mới controllers phòng ban và view index

---
+++ NKSLK_CS/Controllers/PhongBansController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NKSLK_CS.Controllers
+{
+    public class PhongBansController : Controller
+    {
+        // GET: PhongBans
+        public ActionResult Index()
+        {
+            PhongBanList strPB = new PhongBanList();
+            List<PhongBan> obj = strPB.getPhongBan(string.Empty);
+            return View(obj);
+        }
+    }
+}
