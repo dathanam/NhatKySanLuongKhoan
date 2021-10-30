b429303da498c68b3063b2b7105b185c3893d2dc
thuccd <62067426+thuccd@users.noreply.github.com>
2021-10-31 06:36:26 +0700

    Bổ sung nghiệp vụ model Phòng Ban

---
+++ NKSLK_CS/Models/DBConecttion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace NKSLK_CS.Models
+{
+    public class DBConecttion
+    {
+        string strCon;
+        public DBConecttion()
+        {
+            strCon = ConfigurationManager.ConnectionStrings["NKSLK_Context"].ConnectionString;
+
+        }
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(strCon);
+        }
+    }
+}
--- NKSLK_CS/Models/PhongBan.cs
+++ NKSLK_CS/Models/PhongBan.cs
@@ -1,10 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 namespace NKSLK_CS
 {
+    using NKSLK_CS.Models;
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data;
     using System.Data.Entity.Spatial;
+    using System.Data.SqlClient;
+    using System.Linq;
+    using System.Web;
 
     [Table("PhongBan")]
     public partial class PhongBan
@@ -24,4 +33,55 @@ namespace NKSLK_CS
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<CongNhan> CongNhan { get; set; }
     }
+
+    class PhongBanList
+    {
+
+        // ThucCD Phong Ban 
+        DBConecttion db;
+        public PhongBanList()
+        {
+            db = new DBConecttion();
+        }
+
+        // Phuong Thuc Lay database 
+
+        public List<PhongBan> getPhongBan(string id)
+        {
+            string sql;
+            if (string.IsNullOrEmpty(id))
+            {
+                sql = "SELECT * FROM PhongBan";
+            }
+            else
+            {
+                sql = "SELECT * FROM PhongBan WHERE id = " + id;
+            }
+            List<PhongBan> listPhongBan = new List<PhongBan>();
+            SqlConnection connection = db.GetConnection();
+            SqlDataAdapter cmd = new SqlDataAdapter(sql, connection);
+            DataTable dt = new DataTable();
+
+            // open connect :
+            connection.Open();
+            // do tat ca du lieu vao datatable
+            cmd.Fill(dt);
+            // dong ket noi
+            cmd.Dispose();
+            connection.Close();
+
+            PhongBan phongBan;
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                phongBan = new PhongBan();
+                phongBan.id = Convert.ToInt32(dt.Rows[i]["ID"].ToString());
+                phongBan.ten = dt.Rows[i]["Ten"].ToString();
+
+                listPhongBan.Add(phongBan);
+            }
+            return listPhongBan;
+        }
+
+    }
+
 }
