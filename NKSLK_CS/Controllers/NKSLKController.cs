2c98efe7eba2f43299412c67a18c9fca22b3efed
ThanhDat <thanhdatmta99@gmail.com>
2021-10-30 18:21:09 +0700

    fix ok

---
+++ NKSLK_CS/Controllers/NKSLKController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NKSLK_CS.Controllers
+{
+    public class NKSLKController : Controller
+    {
+        // GET: NKSLK
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
